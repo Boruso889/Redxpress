2354963b237579a1e521a50e3c7322ca1e22e7b2
a <ygvmplorez@gmail.com>
2024-02-09 11:58:53 +0500

    dsa

--- app/src/main/java/com/example/redxpress/MainActivity.kt
+++ app/src/main/java/com/example/redxpress/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.redxpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        object : CountDownTimer(1500, 1000) {
+            override fun onTick(p0: Long) {
+            }
+
+            override fun onFinish() {
+                val intent = Intent(this@MainActivity, RegActivity::class.java)
+                startActivity(intent)
+                this@MainActivity.finish()
+
+
+            }
+        }.start()
     }
 }
