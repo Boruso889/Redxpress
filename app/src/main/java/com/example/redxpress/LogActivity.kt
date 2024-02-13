5443ed4dc051e1111402a39d6590bdd7626f6f41
a <ygvmplorez@gmail.com>
2024-02-13 11:18:33 +0500

    hghg

--- app/src/main/java/com/example/redxpress/LogActivity.kt
+++ app/src/main/java/com/example/redxpress/LogActivity.kt
@@ -1,11 +1,39 @@
 package com.example.redxpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
+
+        val tvSignIn = findViewById<TextView>(R.id.TVsignin)
+        val tvLogBtn = findViewById<TextView>(R.id.tvLogBtn)
+        val edEmailLog = findViewById<EditText>(R.id.edEmailLog)
+        val edPassLog = findViewById<EditText>(R.id.edPassLog)
+        val tvRemPass = findViewById<TextView>(R.id.tvRemPass)
+
+        tvLogBtn.setOnClickListener{
+            if (edEmailLog.text.isEmpty() || edPassLog.text.isEmpty())
+                Toast.makeText(this@LogActivity, "Не все поля заполнены", Toast.LENGTH_LONG).show()
+            return@setOnClickListener //alarm
+        }
+        tvLogBtn.setOnClickListener{
+            if (!edEmailLog.text.contains("@"))
+                Toast.makeText(this@LogActivity, "Почта неправильно указана", Toast.LENGTH_LONG).show()
+            return@setOnClickListener // alarm
+        }
+        tvRemPass.setOnClickListener{
+            val intent = Intent(this@LogActivity, PasswordActivity::class.java)
+            startActivity(intent)
+            this@LogActivity.finish()
+        }
+
     }
 }
--- app/src/main/java/com/example/redxpress/RegActivity.kt
+++ app/src/main/java/com/example/redxpress/RegActivity.kt
@@ -1,8 +1,12 @@
 package com.example.redxpress
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
@@ -18,6 +22,10 @@ class RegActivity : AppCompatActivity() {
         val etPassAgain: EditText = findViewById(R.id.etPassAgain)
         val etEmail: EditText = findViewById(R.id.etEmail)
         val regBtn: TextView = findViewById(R.id.regBtn)
+        val cbReg = findViewById<CheckBox>(R.id.cbReg)
+        val TVsignin = findViewById<TextView>(R.id.TVsignin)
+        val ivHide = findViewById<ImageView>(R.id.ivHide)
+        val ivHide2 = findViewById<ImageView>(R.id.ivHide2)
 
         regBtn.setOnClickListener {
             if (etPhone.text.isEmpty() || etName.text.isEmpty() || etPass.text.isEmpty() || etPassAgain.text.isEmpty() || etEmail.text.isEmpty()) { // пустые поля поиск
@@ -29,9 +37,20 @@ class RegActivity : AppCompatActivity() {
                     .show()
                 return@setOnClickListener // alarm cancel
             }
-
+            if (!cbReg.isChecked) {
+                Toast.makeText(this@RegActivity, "Not Accepted", Toast.LENGTH_LONG).show()
+                return@setOnClickListener // alarm cancel
+            }
+            if (etPass != etPassAgain){
+                Toast.makeText(this@RegActivity, "Пароли не одинаковы", Toast.LENGTH_LONG).show()
+                return@setOnClickListener// alarm
+                }
         }
-
+        TVsignin.setOnClickListener {
+            val intent = Intent(this@RegActivity, LogActivity::class.java)
+            startActivity(intent)
+            this@RegActivity.finish()
+        }
 
     }
 }
